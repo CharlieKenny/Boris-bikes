978ccae0577223c3414dd364d8133434ff870a3d
Charlie <crck@outlook.com>
2015-04-29 18:29:27 +0100

    [unstable] uninitialized constant DockingStation::Bike

---
+++ spec/feature/user_accesses_docking_station_spec.rb
@@ -0,0 +1,7 @@
+feature 'user accesses docking station' do 
+	scenario 'docking station releases working bike' do 
+		docking_station = DockingStation.new
+		bike = docking_station.release_bike
+		expect(bike.working?).to eq true
+	end
+end
